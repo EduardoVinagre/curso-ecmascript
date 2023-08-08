573a174256695ca8d147856b3ec9b62e408a2f07
Eduardo Vinagre <eduardovinagre@gmail.com>
2023-08-08 15:32:43 -0600

    Exponential and includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1,3,4,2,6,7,8]
+
+console.log(numbers.includes(4))
+console.log(numbers.includes(10))
+    
+
+const list = ['Oscar', 'David', 'Ana']
+console.log(list.includes('Oscar'))
+console.log(list.includes('Juan'))
