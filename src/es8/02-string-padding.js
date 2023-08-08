cb5eb7e40f64221ceb6ab83dc2051d4f05d0750e
Eduardo Vinagre <eduardovinagre@gmail.com>
2023-08-08 15:52:51 -0600

    trailing commas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,9 @@
+const string = 'Hello';
+
+console.log(string.padStart(7,'_'));
+console.log(string.padStart(5,'_'));
+console.log(string.padStart(6,'_'));
+console.log(string.padStart(16,'_'));
+
+console.log(string.padEnd(6,'_'))
+console.log(string.padEnd(16,'_'))
