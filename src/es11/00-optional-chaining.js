7cfd55593097b30477351b2de187636fcc2e611c
Eduardo Vinagre <eduardovinagre@gmail.com>
2023-08-09 11:17:28 -0600

    Optional chainining

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,13 @@
+const  users = {
+    gndx: {
+        country: 'MX'
+    },
+    ana: {
+        country: 'CO'
+    }
+}
+
+console.log(users.gndx.country)
+console.log(users.gndx.age) //undefined
+
+console.log(users?.bebeloper?.country)
